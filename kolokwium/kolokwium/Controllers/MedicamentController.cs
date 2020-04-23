b9e578646ab9dbdc2178af55de9b63b1e76e26d4
s18849 <s18849@pjwstk.edu.pl>
2020-04-23 10:29:21 +0200

    dodanie kontrolera

---
+++ kolokwium/kolokwium/Controllers/MedicamentController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace kolokwium.Controllers
+{
+    [ApiController]
+    [Route("api/prescriptions")]
+    public class MedicamentController : ControllerBase
+    {
+        
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
